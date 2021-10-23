c9503d8332602f9f35d203e0c659cceff50ef5a6
Mohamad Salimi <mohamad.salimi@hotmail.com>
2021-10-23 19:40:12 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+let share = document.querySelector(".share");
+let socialMedia = document.querySelector(".social-media");
 
+if(window.matchMedia('screen and (min-width: 1000px)').matches){
+    function myFunction() {
+        socialMedia.classList.toggle("active");
+    }
+            
+}else{
+    console.log("phone");
+}
